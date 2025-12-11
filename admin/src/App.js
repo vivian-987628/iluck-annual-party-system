88c22f3ee13ae3266cad1a82ccf08f5ee673ccc2
vivian-987628 <136374313@qq.com>
2025-12-11 09:32:40 +0800

    Add files via upload

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,30 +2,39 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
-import { AuthProvider } from './contexts/AuthContext';
-import Layout from './components/Layout';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Employees from './pages/Employees';
 import Lottery from './pages/Lottery';
 import Games from './pages/Games';
+import Layout from './components/Layout';
+import ProtectedRoute from './components/ProtectedRoute';
+import { AuthProvider } from './contexts/AuthContext';
+import './App.css';
 
 function App() {
   return (
     <ConfigProvider locale={zhCN}>
       <AuthProvider>
         <Router>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Navigate to="/dashboard" replace />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="employees" element={<Employees />} />
-              <Route path="lottery" element={<Lottery />} />
-              <Route path="games" element={<Games />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/login" replace />} />
-          </Routes>
+          <div className="App">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/" element={
+                <ProtectedRoute>
+                  <Layout>
+                    <Routes>
+                      <Route index element={<Navigate to="/dashboard" replace />} />
+                      <Route path="dashboard" element={<Dashboard />} />
+                      <Route path="employees" element={<Employees />} />
+                      <Route path="lottery" element={<Lottery />} />
+                      <Route path="games" element={<Games />} />
+                    </Routes>
+                  </Layout>
+                </ProtectedRoute>
+              } />
+            </Routes>
+          </div>
         </Router>
       </AuthProvider>
     </ConfigProvider>
